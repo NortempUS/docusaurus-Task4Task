2d792c28953c08e2bd8944ce8fbe87bf8046a6da
Marchabar <marchabar1@alum.us.es>
2024-03-16 10:08:25 +0100

    fix: blog entry

--- Task4Task/docusaurus.config.js
+++ Task4Task/docusaurus.config.js
@@ -110,10 +110,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
